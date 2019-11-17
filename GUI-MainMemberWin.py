df38a51a1e2112e6be388c97c43025deabae099e
winpasit <42579006+winpasit@users.noreply.github.com>
2019-11-17 23:14:35 +0700

    Add sample login

--- GUI-MainMemberWin.py
+++ GUI-MainMemberWin.py
@@ -4,9 +4,15 @@ class RootWin() :
     def __init__(self) :
         root = Tk()
         root.title("Member Main Menu")
+        global user
+        user = "none"
         
         header = Label(root, text="Main Menu")
         header.pack()
+        
+        global loginButton
+        loginButton = Button(root, text = "Login",command=self.popLogin)
+        loginButton.pack(side=TOP)
         
         searchProductButton = Button(root, text="Search Product by Branch Name and  Product Name", command=self.popSearchProductWin)
         searchProductButton.pack(side=TOP)
@@ -21,6 +27,40 @@ class RootWin() :
         s1 = SearchProductWin("Product Search")
     def popSearchPromotionWin(self) :
         s2 = SearchPromotionWin("Promotion Search")
+    def popLogin(self) :
+        l1 = LoginWindow("Login")
+
+class LoginWindow() :
+    def __init__ (self,title):
+        self.cwin = Toplevel()
+        self.cwin.title(title)
+        self.cwin.geometry('300x200')
+
+        self.label_username = Label(self.cwin,text="Username :")
+        self.label_password = Label(self.cwin,text="Password :")
+
+        self.entry_username = Entry(self.cwin)
+        self.entry_password = Entry(self.cwin)
+        
+        #for testing only
+        def login():
+            if self.entry_username.get() == "member" and self.entry_password.get() == "member":
+                user = "member"
+                print(user)
+                loginButton.config(text=user)
+                self.cwin.destroy()
+            else:
+                loginButton.config(text="Error")
+                print("Error")
+                self.cwin.destroy()
+
+        self.button_login = Button(self.cwin,text="Login",command=login) # need to change to real login compare from database
+
+        self.label_username.grid(row=0,column=0)
+        self.entry_username.grid(row=0,column=1)
+        self.label_password.grid(row=1,column=0)
+        self.entry_password.grid(row=1,column=1)
+        self.button_login.grid(row=2,column=1)
 
 class SearchPromotionWin() :
     def __init__(self,title):
