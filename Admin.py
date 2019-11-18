30491b6acfdd41756c14d9b32a5fac8acb5913b9
Boss <wutipong1234@hotmail.com>
2019-11-18 21:50:42 +0700

    Rent Table missing column

--- Admin.py
+++ Admin.py
@@ -1,3 +1,6 @@
+import sys
+import tkinter as tk
+from tkinter import ttk
 from tkinter import *
 
 class AdminWindow() :
@@ -16,7 +19,7 @@ class AdminWindow() :
         #for testing only need to send memberID to next step
         #for testing username = 'member' password = 'member' to login
         def login():
-            if self.entry_branch.get() == "salaya":
+            if self.entry_branch.get() == "":
                 self.cwin.destroy()
                 m1 = MenuWin()
 
@@ -35,6 +38,10 @@ class MenuWin() :
 
         def BranchDetailWin() :
             self.root.destroy()
+
+        def RentPlacePop():
+            self.root.destroy()
+            rp = RentPlaceWin()
         
         header = Label(self.root, text="Main Menu")
         header.pack()
@@ -45,7 +52,7 @@ class MenuWin() :
         WarehouseButton = Button(self.root, text="Warehouse")
         WarehouseButton.pack(side=TOP)
 
-        RentPlaceButton = Button(self.root, text="Rent Place")
+        RentPlaceButton = Button(self.root, text="Rent Place", command=RentPlacePop)
         RentPlaceButton.pack(side=TOP)
 
         PromotionButton = Button(self.root, text="Promotion")
@@ -63,6 +70,43 @@ class MenuWin() :
 
 
         self.root.mainloop()
+
+class RentPlaceWin():
+    def __init__ (self):
+        self.root = tk.Tk()
+        self.root.title("Rent Place")
+        #self.root.geometry('700x350')
+
+        def returnToMenu():
+            self.root.destroy()
+            mn = MenuWin()
+
+        def show():
+            tempList = [['rent', '0.72'], ['-', '0.67'], ['-', '0.67'], ['-', '0.5']]
+            #tempList.sort(key=lambda e: e[1], reverse=True)
+
+            for i, (name, score) in enumerate(tempList, start=1):
+                listBox.insert("", "end", values=(i, name, score))
+                
+
+        #scores = tk.Tk() 
+        label = tk.Label(self.root, text="Rent Place", font=("Arial",30)).grid(row=0, columnspan=3)
+        # create Treeview with 3 columns
+        cols = ('Rent Place ID', 'status', 'ID', 'Fname', 'Lname', 'Phone', 'start', 'expire', 'cost')
+        listBox = ttk.Treeview(self.root, columns=cols, show='headings')
+        # set column headings
+        for col in cols:
+            listBox.heading(col, text=col)
+        listBox.grid(row=1, column=0, columnspan=2)
+
+        showScores = tk.Button(self.root, text="Show scores", width=15, command=show).grid(row=4, column=0)
+        BackButton = tk.Button(self.root, text="Back", width=15, command=returnToMenu).grid(row=4, column=1)
+
+
+        self.root.mainloop()
+
+
+
 
 
 Mainmenu = AdminWindow()
